85fc4a470771dbdc70ad5adb8265f3f4abfce78f
peiwal <peiwal@yandex.ru>
2020-07-24 10:46:46 +0400

    add rest connection timeout

--- app/src/main/java/petrov/ivan/deliverymobile/modules/OkHttpClientModule.kt
+++ app/src/main/java/petrov/ivan/deliverymobile/modules/OkHttpClientModule.kt
@@ -5,8 +5,10 @@ import dagger.Provides
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import petrov.ivan.deliverymobile.AppConstants
 import petrov.ivan.deliverymobile.BuildConfig
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 @Module(includes = arrayOf(ContextModule::class))
 class OkHttpClientModule {
@@ -18,12 +20,14 @@ class OkHttpClientModule {
                 .cache(null)
                 .addInterceptor(authInterceptor())
                 .addInterceptor(httpLoggingInterceptor())
+                .connectTimeout(AppConstants.CONNECTION_TIMEOUT_SEC, TimeUnit.SECONDS)
                 .build()
         } else {
             return OkHttpClient().newBuilder()
                 .cache(null)
                 .addInterceptor(httpLoggingInterceptor())
                 .addInterceptor(authInterceptor())
+                .connectTimeout(AppConstants.CONNECTION_TIMEOUT_SEC, TimeUnit.SECONDS)
                 .build()
         }
     }
@@ -41,7 +45,6 @@ class OkHttpClientModule {
         return Interceptor { chain ->
             val newUrl = chain.request().url()
                 .newBuilder()
-                //.addQueryParameter("api_key", AppConstants.API_KEY)
                 .build()
 
             val newRequest = chain.request()
